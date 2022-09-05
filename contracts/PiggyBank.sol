048bb75969f0a1ff705a48b8af79862d6a6c285e
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2022-09-05 17:26:50 +0200

    Update

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract PiggyBank {}
+contract PiggyBank {
+    event Deposite(uint indexed amount);
+    event Withdraw(uint indexed amount);
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {}
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+
+    function withdraw() external onlyOwner {
+        emit Withdraw(address(this).balance);
+        selfdestruct(payable(owner));
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
